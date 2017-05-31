c3597c99ae11d96be60362e054d8053a86a842ce
denglin <denglin@rrjc.com>
2017-05-31 17:11:34 +0800

    添加依赖

---
+++ app/src/main/java/com/oldnum7/adpter/base/animation/BaseAnimation.java
@@ -0,0 +1,13 @@
+package com.oldnum7.adpter.base.animation;
+
+import android.animation.Animator;
+import android.view.View;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public interface  BaseAnimation {
+
+    Animator[] getAnimators(View view);
+
+}
---
+++ app/src/main/java/com/oldnum7/adpter/base/entity/MultiItemEntity.java
@@ -0,0 +1,10 @@
+package com.oldnum7.adpter.base.entity;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public interface MultiItemEntity {
+
+    int getItemType();
+
+}
